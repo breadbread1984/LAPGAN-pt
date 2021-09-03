84afa4b535350c3ed74afcaa5ddc7f7645787413
breadbread1984 <breadbread1984@163.com>
2021-09-03 10:32:52 +0800

    update

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ def main():
   parser = argparse.ArgumentParser();
   parser = pl.Trainer.add_argparse_args(parser);
   parser = Trainer.add_model_specific_args(parser);
-  parser.add_argument('--batch_size', type = int, default = 32);
+  parser.add_argument('--batch_size', type = int, default = 256);
   parser.add_argument('--num_workers', type = int, default = 8);
   parser.add_argument('--download', type = bool, default = False);
   args = parser.parse_args();
