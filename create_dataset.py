40076441d8daaa3e6b275485bda5a61b5e825b99
Konquer your Mind <breadbread1984@163.com>
2021-10-11 21:55:30 +0800

    Update create_dataset.py

--- create_dataset.py
+++ create_dataset.py
@@ -47,7 +47,7 @@ class CIFAR10Dataset(pl.LightningDataModule):
     return testset;
   def val_dataloader(self,):
     valset = CIFAR10(root = 'cifar10', train = False, download = self.args.download, transform = self.transform);
-    valsett = DataLoader(valset, batch_size = self.args.batch_size, shuffle = True, num_workers = self.args.num_workers);
+    valset = DataLoader(valset, batch_size = self.args.batch_size, shuffle = True, num_workers = self.args.num_workers);
     return valset;
 
 if __name__ == "__main__":
